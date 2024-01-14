7693dac45c14d29bccef3062885e2d30bdb110cd
VladimirIvanovLordi <vladimir_ivanov_01@abv.bg>
2024-01-14 21:49:04 +0200

    complete the repository for location add repositories for holiday and reservation

--- src/main/java/uniplovdiv/webserverapps/travelagencyapi/api/repositories/HolidayRepository.java
+++ src/main/java/uniplovdiv/webserverapps/travelagencyapi/api/repositories/HolidayRepository.java
@@ -2,8 +2,8 @@ package uniplovdiv.webserverapps.travelagencyapi.api.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import uniplovdiv.webserverapps.travelagencyapi.api.models.Location;
+import uniplovdiv.webserverapps.travelagencyapi.api.models.Holiday;
 
 @Repository
-public interface LocationRepository extends CrudRepository<Location, Long> {
+public interface HolidayRepository extends CrudRepository<Holiday, Long> {
 }
--- src/main/java/uniplovdiv/webserverapps/travelagencyapi/api/repositories/ReservationRepository.java
+++ src/main/java/uniplovdiv/webserverapps/travelagencyapi/api/repositories/ReservationRepository.java
@@ -2,8 +2,7 @@ package uniplovdiv.webserverapps.travelagencyapi.api.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import uniplovdiv.webserverapps.travelagencyapi.api.models.Location;
 
 @Repository
-public interface LocationRepository extends CrudRepository<Location, Long> {
+public interface ReservationRepository extends CrudRepository<ReservationRepository, Long> {
 }
